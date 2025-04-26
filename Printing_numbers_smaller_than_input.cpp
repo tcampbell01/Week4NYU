d0b9f473280cfcd905e54278eb3ba93add4533dc
tcampbell01 <teresa.j.campbell@gmail.com>
2025-04-26 13:30:46 -0400

    printing_smaller_numbers

---
+++ Printing_numbers_smaller_than_input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input_number;
+    int current_number = 0;
+
+    cout << "Please enter a whole number: "<< endl;
+    cin >> input_number;
+    cout << "You chose number: " << input_number << endl;
+
+    cout << "Here are all the numbers that are smaller than the number you chose, starting with 0: " << endl;
+    while (current_number < input_number) {
+        cout << current_number << endl;
+        current_number = current_number + 1;
+    }
+
+    return 0;
+
+}
+
